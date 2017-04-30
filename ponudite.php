cb0c0e3249ca910fb75d37dcdb3bded96026c011
ankanedic1983 <ankanedic1983@gmail.com>
2017-04-30 08:46:12 +0200

    Create ponudite.php

---
+++ ponudite.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Berza bilja: Ponudite </title>
+<link href = "berzastyle.css" rel = "stylesheet" type = "text/css">
+</head>
+<body class="body">
+
+<h2 id= "welcome">Dobrodošli!<br><h2>
+<img id = "slika" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzoTE7Xqj4XhDY2ZyiGrzvuyVECZoOpITxlwVio7TelDUV6VMCrQ"/>
+
+<ul>
+	<li><a href="berza1.php">Početna</a></li>
+	<li><a href="berza2.php">Ponudite</a></li>
+	<li><a href="pretraga.php">Pronadjite</a></li>
+	<li><a href="">Iskustva</a></li>
+</ul>	
+
+<h1 id="naslov">Berza bilja<br><h1>
+
+<form method="post" action="berzaphp.php">
+
+<p>Ime: <input type="text" name="ime"></p>
+<p>Lokacija: <input type="text" name="lokacija"></p>
+<p>Telefon: <input type="text" name="telefon"></p>
+<p>Biljka: <input type="text" name="biljka"></p>
+<p>Količina: <input type="text" name="kolicina"></p>
+<p>Cena po kg: <input type="text" name="cena"></p>
+<p><input type = "submit"></p>
+
+</form>
