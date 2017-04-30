ab3f7dc80a546c6883f6f1ac420fe6deeedabe70
ankanedic1983 <ankanedic1983@gmail.com>
2017-04-30 08:57:06 +0200

    Create pronadjite.php

---
+++ pronadjite.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Berza bilja: Pronađite</title>
+<link href = "berzastyle.css" rel = "stylesheet" type = "text/css">
+</head>
+<body class="body">
+
+<h2 id= "welcome">Dobrodošli!<br><h2>
+<img id = "slika" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzoTE7Xqj4XhDY2ZyiGrzvuyVECZoOpITxlwVio7TelDUV6VMCrQ"/>
+
+<ul>
+	<li><a href="pocetna.php">Početna</a></li>
+	<li><a href="ponudite.php">Ponudite</a></li>
+	<li><a href="pronadjite.php">Pronadjite</a></li>
+	<li><a href="">Iskustva</a></li>
+</ul>	
+
+<h1 id="naslov">Berza bilja<br><h1>
+<form method="post" action = "rezultati.php">
+<p>Biljka: <input type="text" name="biljka"></p>
+<p>Lokacija: <input type="text" name="lokacija"></p>
+<p>Ako ostavite polje biljka prazno izlistace se sve biljke</p>
+<p>Ako ostavite polje lokacija prazno izlistace se sve lokacije</p>
+<p><input type = "submit"></p>
+</form>
