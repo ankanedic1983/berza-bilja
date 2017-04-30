f67b937bbe0a699bf9d9f91c27812992b3ab11eb
ankanedic1983 <ankanedic1983@gmail.com>
2017-04-30 08:44:36 +0200

    Create pocetna.php

---
+++ pocetna.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Berza bilja: Pocetna</title>
+<link href = "berzastyle.css" rel = "stylesheet" type = "text/css">
+</head>
+<body class="body">
+
+<h2 id= "welcome">Dobrodošli!<br><h2>
+<img id = "slika" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzoTE7Xqj4XhDY2ZyiGrzvuyVECZoOpITxlwVio7TelDUV6VMCrQ"/>
+
+<ul>
+	<li><a href="berza1.php">Početna</a></li>
+	<li><a href="berza2.php">Ponudite</a></li>
+	<li><a href="pretraga.php">Pronadjite</a></li>
+	<li><a href="">Iskustva</a></li>
+</ul>	
+
+<h1 id="naslov">Berza bilja<br><h1>
+ 
+
+<p>Ovde možete ponuditi i naći sveže, sušeno i prerađeno bilje
+iz svih delova Srbije.</p>
+<p>Takođe ovo je mesto gde možemo da razmenjujemo iskustva, savete, recepte.
+<br><br><br></p>
+<p>&copy Anka Nedić Stamenović</p>
+</body>
+</html>
